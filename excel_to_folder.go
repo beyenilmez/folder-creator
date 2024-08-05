c2eb29559683d3527c66ebdbe8ab88942059f310
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-08-05 16:32:02 +0300

    add udf file placeholder support

--- excel_to_folder.go
+++ excel_to_folder.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/nguyenthenguyen/docx"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 	"github.com/xuri/excelize/v2"
@@ -58,6 +60,12 @@ func (a *App) GetFileDialog() string {
 	path, err := runtime.OpenFileDialog(a.ctx, runtime.OpenDialogOptions{
 		Title:                "Dosya seçin",
 		CanCreateDirectories: true,
+		Filters: []runtime.FileFilter{
+			{
+				DisplayName: "UDF Dosyası",
+				Pattern:     "*.udf",
+			},
+		},
 	})
 
 	if err != nil {
@@ -208,6 +216,15 @@ func replacePlaceholdersWord(docx *docx.Docx, placeholder, replacement string) e
 	return nil
 }
 
+func replacePlaceholdersUdf(content, placeholder, replacement string) string {
+	titlePlaceholder := "{{" + placeholder + "}}"
+	if strings.Contains(content, titlePlaceholder) {
+		replacement = toTitleCaseWord(replacement)
+		content = strings.ReplaceAll(content, titlePlaceholder, replacement)
+	}
+	return strings.ReplaceAll(content, "{"+placeholder+"}", replacement)
+}
+
 func toTitleCaseFolder(text string) string {
 	words := strings.Split(text, " ")
 	for i, word := range words {
@@ -247,23 +264,18 @@ func (a *App) CreateFolders(excelPath string, wordPath string, copyFolderPath st
 		if copyFolderPath != "" {
 			if err := copyFolderContents(copyFolderPath, targetFolderPath); err != nil {
 				runtime.LogError(a.ctx, "Failed to copy folder contents: "+err.Error())
-				continue
 			}
 		}
 
 		if wordPath != "" {
 			if err := createWordDocument(wordPath, wordFileNamePattern, headers, rows[i], targetFolderPath); err != nil {
 				runtime.LogError(a.ctx, "Failed to create word document: "+err.Error())
-				continue
 			}
 		}
 
 		if filePath != "" {
-			fileName := generatePatternName(fileNamePattern, headers, rows[i])
-
-			if err := copyFile(filePath, filepath.Join(targetFolderPath, fileName)); err != nil {
-				runtime.LogError(a.ctx, "Failed to copy file: "+err.Error())
-				continue
+			if err := createUdfDocument(filePath, fileNamePattern, headers, rows[i], targetFolderPath); err != nil {
+				runtime.LogError(a.ctx, "Failed to create udf document: "+err.Error())
 			}
 		}
 
@@ -346,6 +358,127 @@ func createWordDocument(filePath string, wordFileNamePattern string, headers []s
 	return nil
 }
 
+func createUdfDocument(filePath string, fileNamePattern string, headers []string, row []string, targetPath string) error {
+	// extract filePath to temp folder (zip)
+	tempFolder := filepath.Join(os.TempDir(), "folder-creator-"+uuid.NewString())
+
+	if err := unzip(filePath, tempFolder); err != nil {
+		runtime.LogError(appContext, "Failed to unzip file: "+err.Error())
+		return err
+	}
+
+	contentPath := filepath.Join(tempFolder, "content.xml")
+
+	if _, err := os.Stat(contentPath); err != nil {
+		runtime.LogError(appContext, "Failed to find content.xml: "+err.Error())
+		return err
+	}
+
+	// find and change content of content.xml
+	content, err := os.ReadFile(contentPath)
+	if err != nil {
+		runtime.LogError(appContext, "Failed to read content.xml: "+err.Error())
+		return err
+	}
+
+	strContent := string(content)
+
+	for i, header := range headers {
+		if i >= len(row) {
+			runtime.LogWarning(appContext, "Row is shorter than headers")
+			break
+		}
+		colCell := sanitizeCellWord(row[i])
+		strContent = replacePlaceholdersUdf(strContent, header, colCell)
+	}
+
+	// write content.xml
+	if err := os.WriteFile(contentPath, []byte(strContent), 0644); err != nil {
+		runtime.LogError(appContext, "Failed to write content.xml: "+err.Error())
+		return err
+	}
+
+	// Generate file name
+	fileNamePattern = strings.TrimSuffix(fileNamePattern, filepath.Ext(fileNamePattern))
+	fileName := generatePatternName(fileNamePattern, headers, row) + ".udf"
+
+	// Create a zip at targetPath
+	if err := createZip(tempFolder, filepath.Join(targetPath, fileName)); err != nil {
+		runtime.LogError(appContext, "Failed to zip folder: "+err.Error())
+		return err
+	}
+
+	// Delete temp folder
+	if err := os.RemoveAll(tempFolder); err != nil {
+		runtime.LogError(appContext, "Failed to delete temp folder: "+err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func createZip(source, target string) error {
+	zipFile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip the source folder itself
+		if path == source {
+			return nil
+		}
+
+		// Create a zip file header
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// Ensure the file path in the zip file is relative to the source folder
+		header.Name = filepath.Base(path)
+
+		// If it's a directory, create the header with a trailing slash
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		// Create a writer for the zip file header
+		writer, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		// If it's a directory, return here
+		if info.IsDir() {
+			return nil
+		}
+
+		// Open the file to be zipped
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		// Copy the file contents to the zip writer
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return err
+}
+
 func copyFile(src, dst string) error {
 	in, err := os.Open(src)
 	if err != nil {
@@ -402,6 +535,8 @@ func copyFolderContentsV2(src, dest string, headers []string, row []string) erro
 
 		if filepath.Ext(relativePath) == ".docx" {
 			return createWordDocument(path, filepath.Base(path), headers, row, dest)
+		} else if filepath.Ext(relativePath) == ".udf" {
+			return createUdfDocument(path, filepath.Base(path), headers, row, dest)
 		}
 
 		relativePath = generatePatternName(relativePath, headers, row)
